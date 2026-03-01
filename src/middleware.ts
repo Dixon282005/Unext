6939f533c84ef66a070a8b26752ba9c103eb682e
Dickdick <dixonramos282005@hotmail.com>
2026-02-28 21:33:08 -0400

    fix middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -49,6 +49,13 @@ export async function middleware(request: NextRequest) {
     return NextResponse.redirect(url)
   }
 
+  // Si el usuario NO está autenticado e intenta ir a dashboard, mándalo a login
+  if (!user && pathname.startsWith('/dashboard')) {
+    const url = request.nextUrl.clone()
+    url.pathname = '/login'
+    return NextResponse.redirect(url)
+  }
+
   return supabaseResponse
 }
 
